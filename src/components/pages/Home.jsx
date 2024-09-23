371a887c73006e9292a44a95459f2b61a07cec3a
Raul Vaida <vaidatc@gmail.com>
2024-09-23 19:52:35 +0300

    feat: add api loading

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import Navbar from "../complex/Navbar";
+import { getPosts } from "../../services/postsService";
+import PostComponent from "../complex/PostComponent";
 
 const Home = () => {
+    const [rows, setRows] = useState([]);
+
+    useEffect(() => {
+        const newRowsArr = [];
+
+        getPosts().then(async (res) => {
+            res.forEach((post) => {
+                newRowsArr.push(<PostComponent key={post.id} post={post} />);
+            });
+
+            setRows(newRowsArr);
+        });
+    }, []);
+
     return (
         <>
             <Navbar />
-            Eu exist
+            {rows.length > 0 ? rows : <p>Loading...</p>}
         </>
     );
 };
