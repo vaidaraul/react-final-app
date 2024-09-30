4f906ae7f602a4ba2c1daf5e7a5210df3b8bd391
Raul Vaida <vaidatc@gmail.com>
2024-09-30 18:36:21 +0300

    feat: added dark mode

--- src/components/complex/Navbar.jsx
+++ src/components/complex/Navbar.jsx
@@ -1,18 +1,36 @@
 import { Link } from "react-router-dom";
 import "../../styling/navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleDark = () => {
+        setDarkMode(!darkMode);
+    };
+
     return (
-        <nav className="navbar">
-            <ul className="navbar-links">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
+        <>
+            <nav
+                className={`navbar ${
+                    darkMode ? "navbar-dark" : "navbar-light"
+                }`}
+            >
+                <ul
+                    className={`navbar-links ${
+                        darkMode ? "navbar-links-dark" : "navbar-links-light"
+                    }`}
+                >
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+            <button onClick={toggleDark}>Switch Mode</button>
+        </>
     );
 };
 
